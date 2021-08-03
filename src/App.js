cdb1f881ab8d07a73fe56f386dfaf8482cff7147
Divy Motisariya <motisariyadivy2@gmail.com>
2021-08-03 16:02:22 +0530

    Components Part - 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { Navbar, NavbarBrand } from "reactstrap";
 // import "./App.css";
 import Menu from "./components/Menu.Component";
 import React, { Component } from "react";
+import { DISHES } from "./shared/dishes.shared";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			dishes: DISHES,
+		};
+	}
+
 	render() {
 		return (
 			<div className="App">
@@ -13,7 +22,7 @@ class App extends Component {
 						<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
 					</div>
 				</Navbar>
-				<Menu />
+				<Menu dishes={this.state.dishes} />
 			</div>
 		);
 	}
