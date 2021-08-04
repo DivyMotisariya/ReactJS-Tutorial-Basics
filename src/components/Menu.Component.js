30b2b3f31d9869cbfa25f14e9984dfb92ae70526
Divy Motisariya <motisariyadivy2@gmail.com>
2021-08-04 15:10:05 +0530

    Functional Components

--- src/components/Menu.Component.js
+++ src/components/Menu.Component.js
@@ -3,12 +3,12 @@ import {
 	Card,
 	CardImg,
 	CardImgOverlay,
-	CardText,
-	CardBody,
+	// CardText,
+	// CardBody,
 	CardTitle,
 } from "reactstrap";
 
-class Menu extends Component {
+/* class Menu extends Component {
 
 	renderDish(dish) {
 		if (dish) {
@@ -46,6 +46,32 @@ class Menu extends Component {
 			</div>
 		);
 	}
+} */
+
+function RenderDish({ dish, onClick }) {
+	return (
+		<Card onClick={() => onClick(dish.id)}>
+			<CardImg width="100%" src={dish.image} alt={dish.name} />
+			<CardImgOverlay>
+				<CardTitle>{dish.name}</CardTitle>
+			</CardImgOverlay>
+		</Card>
+	);
 }
+
+const Menu = (props) => {
+	const menu = props.dishes.map((dish) => {
+		return (
+			<div key={dish.id} className="col-12 col-md-5 m-1">
+				<RenderDish dish={dish} onClick={props.onClick} />
+			</div>
+		);
+	});
+	return (
+		<div className="container">
+			<div className="row">{menu}</div>
+		</div>
+	);
+};
 
 export default Menu;
--- src/components/SelectedDish.Component.js
+++ src/components/SelectedDish.Component.js
@@ -8,7 +8,7 @@ import {
 	Media,
 } from "reactstrap";
 
-class SelectedDish extends Component {
+/* class SelectedDish extends Component {
 	renderComments(comments) {
 		return comments.map((comment) => {
 			return (
@@ -56,6 +56,54 @@ class SelectedDish extends Component {
 			);
 		} else return <div></div>;
 	}
+} */
+
+function RenderComments({ comments }) {
+	return comments.map((comment) => {
+		return (
+			<div key={comment.id}>
+				<Media>
+					<Media body>
+						<p>{comment.comment}</p>
+						<p>
+							-- {comment.author} (
+							{new Intl.DateTimeFormat("en-US", {
+								year: "numeric",
+								month: "short",
+								day: "2-digit",
+							}).format(new Date(Date.parse(comment.date)))}
+							)
+						</p>
+					</Media>
+				</Media>
+			</div>
+		);
+	});
 }
+
+const SelectedDish = (props) => {
+	const dish = props.selectedDish;
+	if (dish) {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-md-5 m-1">
+						<Card>
+							<CardImg width="100%" src={dish.image} alt={dish.name} />
+							<CardBody>
+								<CardTitle>{dish.name}</CardTitle>
+								<CardText>{dish.description}</CardText>
+							</CardBody>
+						</Card>
+					</div>
+					<Media className="col-12 col-md-5 m-1">
+						<Media heading>Comments</Media>
+						<RenderComments comments={dish.comments} />
+					</Media>
+				</div>
+			</div>
+		);
+	} else return <div></div>;
+};
 
 export default SelectedDish;
